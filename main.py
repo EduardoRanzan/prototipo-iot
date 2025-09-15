b4b1e1a8637296981e824702f94d21442e7fc733
Eduardo Ranzan Ferreira <ranzaneduardo03@gmail.com>
2025-09-15 15:55:55 -0300

    Ajuste para teste

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
-import paho.mqtt.client as mqtt
-from influxdb import InfluxDBClient
-import json
+from dotenv import load_dotenv
+import os
 
-# Configuração do InfluxDB
-client = InfluxDBClient(host='localhost', port=8086, database='iot_db')
+load_dotenv()
 
-# Função chamada quando chega uma mensagem MQTT
-def on_message(client_mqtt, userdata, msg):
-    data = json.loads(msg.payload.decode())
-    json_body = [
-        {
-            "measurement": "sensor_data",
-            "tags": {
-                "device_id": data["device_id"],
-                "location": data["location"]
-            },
-            "fields": {
-                "temperature": float(data["temperature"]),
-                "humidity": float(data["humidity"]),
-                "luminosity": int(data["luminosity"])
-            }
-        }
-    ]
-    client.write_points(json_body)
+url = os.getenv("INFLUX_URL")
+token = os.getenv("INFLUX_TOKEN")
+org = os.getenv("INFLUX_ORG")
+bucket = os.getenv("INFLUX_BUCKET")
 
-# Configuração do MQTT
-mqtt_client = mqtt.Client()
-mqtt_client.on_message = on_message
-mqtt_client.connect("localhost", 1883, 60)
-mqtt_client.subscribe("iot/sensors")
-mqtt_client.loop_forever()
+
+from influxdb_client import InfluxDBClient, Point, WriteOptions
+
+client = InfluxDBClient(url=url, token=token, org=org)
+write_api = client.write_api(write_options=WriteOptions(batch_size=1))
+
+point = (
+    Point("umidade")
+    .tag("sensor", "box")
+    .field("valor", 50)
+)
+
+write_api.write(bucket=bucket, record=point)
+print("Teste de inserção")
+
+client.close()
